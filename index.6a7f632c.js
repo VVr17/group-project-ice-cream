ad6f3974e66bd7e5f2c49a6f507bc97bea7035a8
VVr17 <103305401+VVr17@users.noreply.github.com>
2022-06-26 13:17:31 +0000

    Deploying to gh-pages from @ VVr17/group-project-ice-cream@c48d0da3c594fa2c3d249190099f458111faf1ac 🚀

---
+++ index.6a7f632c.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn1:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn1.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.6a7f632c.js.map
