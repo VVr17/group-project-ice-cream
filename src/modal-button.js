75da9f288dbe8c99b8b464992013455ed2da20eb
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2022-06-26 15:53:32 +0300

    fixed modal

--- src/modal-button.js
+++ src/modal-button.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
+     openModalBtn1: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
