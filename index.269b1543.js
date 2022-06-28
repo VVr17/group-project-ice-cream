463816f179c97dff50976008f77f8d45c11d859d
VVr17 <103305401+VVr17@users.noreply.github.com>
2022-06-28 14:12:39 +0000

    Deploying to gh-pages from @ VVr17/group-project-ice-cream@73000e65632e10356d5aee08e13c5ac7f5d818d6 🚀

---
+++ index.269b1543.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn1:document.querySelector("[data-modal-open1]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn1.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.269b1543.js.map
--- index.fd9e1329.js
+++ index.fd9e1329.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn1:document.querySelector("[data-modal-open1]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn1.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.0f365138.js.map
+//# sourceMappingURL=index.fd9e1329.js.map
