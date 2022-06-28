6174e2d51b7200d4e5120320978b734e6745d100
uTatsii <utatsiy@gmail.com>
2022-06-28 19:41:58 +0300

    try to flip it

---
+++ src/js/products.js
@@ -0,0 +1,10 @@
+function addFlip() {
+  var element = document.getElementById("myFlipCard");
+  element.classList.add("flip");
+} 
+
+function backFlip() {
+  var element = document.getElementById("myFlipCard");
+  element.classList.remove("flip");
+} 
+
