b362689b7d31edfed4ff6b44ceed0ac6e4f7e437
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2022-06-28 21:38:21 +0300

    додав посилання на кнопки

--- src/js/modal-button.js
+++ src/js/modal-button.js
@@ -1,13 +1,19 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
-     openModalBtn1: document.querySelector("[data-modal-open1]"),
+    openModalBtn1: document.querySelector("[data-modal-open1]"),
+    openModalBtn2: document.querySelector("[data-modal-open2]"),
+    openModalBtn3: document.querySelector("[data-modal-open3]"),
+        openModalBtn4: document.querySelector("[data-modal-open4]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
+  refs.openModalBtn4.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
