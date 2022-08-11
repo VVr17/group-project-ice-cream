3177041762d9e7bf620aaee1c6b45d2214c1855b
Vira Voronova <vera.voronova@hotmail.com>
2022-08-11 13:57:31 +0200

    updated

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/customer-review-slider';
+import './js/franchise';
+import './js/location';
+import './js/menu';
+import './js/modal-button';
+import './js/modal-slider';
+import './js/products';
+import './js/read-more';
+import './js/scrol';
